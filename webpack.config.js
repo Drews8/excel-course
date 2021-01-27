72b818aebec29e9816511bca67fc744be6dcf13d
Andrii Prokhorenko <prokhorenko.andrii1@gmail.com>
2021-01-27 23:08:22 +0200

    finish project config

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,83 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtructPlugin = require('mini-css-extruct-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+console.log('Is Prod -', isProd);
+console.log('Is Dev -', isDev);
+
+const filename = (ext) => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: './index.js',
+  entry: [
+    '@babel/polyfill',
+    './index.js',
+  ],
   output: {
-    filename: 'bundle.[hash].js',
-    path: path.resolve(__dirname, 'dist')
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: isDev ? 'source-map' : 'eval',
+  devServer: {
+    contentBase: './dist',
+  },
+  target: 'web',
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core'),
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html'
+      template: 'index.html',
     }),
     new CleanWebpackPlugin(),
     new CopyPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, 'src/favicon.ico'),
-          to: path.resolve(__dirname, 'dist')
+          to: path.resolve(__dirname, 'dist'),
         },
       ],
     }),
-    new MiniCssExtructPlugin(
-      //{filename: 'bundle.[hash].css'}
-    )
+    new MiniCssExtractPlugin(
+      { filename: filename('css') },
+    ),
+    new ESLintPlugin(),
   ],
   module: {
-    
-      test: /\.css$/i,
-      use: ["style-loader", "css-loader"],
-    
-  }
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+
+  },
 };
